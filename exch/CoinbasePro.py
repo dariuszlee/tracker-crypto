9da0b54daf6c3c0d7b3ce007db244b29732b75ca
dariuszlee <dariuszlee93@gmail.com>
2018-09-10 11:28:13 -0400

    Cosmetic/Lint changes && Historical data

--- exch/CoinbasePro.py
+++ exch/CoinbasePro.py
@@ -1,9 +1,9 @@
 from ExchangeInterface import ExchangeInterface
 
 from urllib.parse import urlencode
-from tornado import ioloop, httpclient
+from tornado import httpclient
 
-from json import loads, dumps
+from json import loads
 
 from datetime import datetime, timedelta
 
@@ -12,34 +12,34 @@ class CoinbaseProExchange(ExchangeInterface):
     __Data = None
 
     def __init__(self):
-        if CoinbaseProExchange.__Data == None:
+        if CoinbaseProExchange.__Data is None:
             CoinbaseProExchange.__Data = self.__loadData__(self.get_exchname())
         super().__init__(self.__Data)
 
     def get_current_price(self, globalCurrency):
         localCurrency = self.get_local_exchange(globalCurrency)
         price = self.get_trades(localCurrency)[0].get('price')
-        if price == None:
+        if price is None:
             return 0
         return price
-        
+
     def get_price_at(self, currency, time):
         return 0
 
     def get_historical_dump(self):
-        return 0
+        past = datetime(2018, 8, 22, 11, 0)
+        pres = past + timedelta(seconds=granularity * 300)
+        data = self.get_candles('btc-usd', past, pres)
+        print(data)
 
-    def get_candles(self, currency, start = None, end = None, granularity = 60):
+    def get_candles(self, currency, start=None, end=None, granularity=60):
         localCurrency = self.get_local_exchange(currency)
-        utcDiff = datetime.utcnow() - datetime.now()
 
         url = self.__get_candles_url(localCurrency, start, end, granularity)
         response = self.get_data(url)
-        data = [ { 'time' : datetime.fromtimestamp(f[0]), 'close' : f[4], 'volume' : f[5] } for f in response ]
-        # Verify dates
-        if start != None and end != None:
-            if data[-1]['time'] != end
-
+        data = [{'time': datetime.fromtimestamp(f[0]), 'close': f[4],
+                 'volume': f[5]} for f in response]
+        return data
 
     def get_trades(self, localCurrency):
         url = self.__get_trade_url('trades', localCurrency)
@@ -51,9 +51,9 @@ class CoinbaseProExchange(ExchangeInterface):
 
     def __get_candles_url(self, cur, start, end, granularity):
         paramsToEncode = {}
-        if start != None and end != None:
+        if start is not None and end is not None:
             paramsToEncode['start'] = start
-            paramsToEncode['end'] = end 
+            paramsToEncode['end'] = end
         paramsToEncode['granularity'] = granularity
         paramsEncoded = urlencode(paramsToEncode)
 
@@ -70,12 +70,14 @@ class CoinbaseProExchange(ExchangeInterface):
         client = httpclient.HTTPClient()
         httpmethod = "GET"
         # Ugh... add Some user-agent. Api specific issue
-        request = httpclient.HTTPRequest(url, method=httpmethod, headers={ 'user-agent' : 'tornado' })
+        request = httpclient.HTTPRequest(url, method=httpmethod,
+                                         headers={'user-agent': 'tornado'})
         response = client.fetch(request)
         return loads(response.body)
 
     def get_exchname(self):
         return 'coinbase-pro'
+
 
 if __name__ == '__main__':
     granularity = 3600
@@ -87,4 +89,5 @@ if __name__ == '__main__':
 
     actualTime = pres - timedelta(seconds=granularity * 5)
     actualPast = past - timedelta(seconds=granularity * 5)
-    data = cbPro.get_candles(currency, past.isoformat(), pres.isoformat(), granularity)
+    data = cbPro.get_candles(currency, past.isoformat(),
+                             pres.isoformat(), granularity)
